e4d103cb0913d70335adf252f38651c313b50522
Paul <paulbyrne99@rocketmail.com>
2020-05-05 01:46:53 +0100

    Vigenere cipher added

---
+++ vigenere.jl
@@ -0,0 +1,36 @@
+# A Julia implementation of a Vigenere cipher
+
+function vigenere()
+    #Get plaintext and key
+    println("Please enter the text: ")
+    text = readline()
+    println("Please enter the key: ")
+    key = readline()
+    #Key must be of equal length to plaintext
+    while length(key) != length(text)
+        println("Error: key must be the same length as the text: ", length(text))
+        key = readline()
+    end
+
+    #The original cipher operated in uppercase, so I feel it is appropriate here
+    text = uppercase(text)
+    key = uppercase(key)
+
+    #=Now we have a suitable text and key, time to encrypt. Index points to the current position in the Key
+    A caesar cipher is carried out each time, where A corresponds to the current position in the Key
+    Julia is 1 indexed! =#
+    index = 1
+    for char in text
+        distance = key[index] - 'A'
+        newchar = char + distance
+        if(newchar > 'Z')
+            newchar -= 26
+        end
+        if(newchar < 'A')
+            newchar += 26
+        end
+        print(newchar)
+    end
+
+end
+vigenere()
