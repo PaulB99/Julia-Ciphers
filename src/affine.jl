d7f78ef9aa2c7e78a61a2cb59b7ab516baf9dbef
Paul <paulbyrne99@rocketmail.com>
2020-06-07 00:24:07 +0100

    input working

--- src/affine.jl
+++ src/affine.jl
@@ -1,4 +1,22 @@
 # A Julia implementation of the Affine ciphertext
 
+# Main function
 function affine()
-    
+
+    m = 26 # The number of letters in the alphabet used
+    println("Please input the plaintext")
+    plaintext = readline()
+    plaintext = uppercase(plaintext)
+    plaintext = replace(plaintext, " " => "")
+
+    println("Please enter the a value")
+    a = parse(Int, readline())
+    while a % m != 0
+        println("a value invalid, must be coprime with m value " * string(m))
+        a = parse(Int, readline())
+    end
+    println("Please enter the b value")
+    b = parse(Int, readline())
+end
+
+affine()
