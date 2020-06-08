488ada6f873a6038b4c31ba9bc0f7288892ecd59
Paul <paulbyrne99@rocketmail.com>
2020-06-08 13:24:36 +0100

    started on baconian

---
+++ src/baconian.jl
@@ -0,0 +1,29 @@
+# An implementation of the baconian cipher in Julia
+
+function baconian()
+
+    # The key for each letter
+    A = "aaaaa"    I = "abaaa"    R = "baaaa"
+    B = "aaaab"    K = "abaab"    S = "baaab"
+    C = "aaaba"    L = "ababa"    T = "baaba"
+    D = "aaabb"    M = "ababb"    U = "baabb"
+    E = "aabaa"    N = "abbaa"    W = "babaa"
+    F = "aabab"    O = "abbab"    X = "babab"
+    G = "aabba"    P = "abbba"    Y = "babba"
+    H = "aabbb"    Q = "abbbb"    Z = "babbb"
+
+    # Take input
+
+    println("Please input the secret message")
+    secret = readline()
+    secret = uppercase(secret)
+    secret = replace(secret, "V" => "U")
+    secret = replace(secret, "J" => "I")
+
+    println("Please input the normal text (5 times longer than the secret)")
+    text = readline()
+    while length(text) != 5 * length(secret)
+        println("Not 5 times the length of the secret. Try again")
+        text = readline()
+    end
+    text = lowercase(text)
