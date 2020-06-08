f405a560d3b1242d7820ba0e9c70fd9faab4fee9
Paul <paulbyrne99@rocketmail.com>
2020-06-08 19:21:25 +0100

    complete but not working properly

--- src/baconian.jl
+++ src/baconian.jl
@@ -3,15 +3,15 @@
 function baconian()
 
     # The key for each letter
-    keys = ["aaaaa", "aaaab", "aaaba", "aaabb", "aabaa", "aabab", "aabba", "aabbb", "abaaa", "abaab", "ababa", "ababb", "abbaa", "abbab", "abbba", "abbbb", "baaaa", "baaab", "baaba", "baabb", "babaa", "babab", "babba", "babbb"]    
-    A = "aaaaa"    I = "abaaa"    R = "baaaa"
+    keys = ["aaaaa", "aaaab", "aaaba", "aaabb", "aabaa", "aabab", "aabba", "aabbb", "abaaa","abaaa", "abaab", "ababa", "ababb", "abbaa", "abbab", "abbba", "abbbb", "baaaa", "baaab", "baaba", "baabb","baabb", "babaa", "babab", "babba", "babbb"]
+#=  A = "aaaaa"    I = "abaaa"    R = "baaaa"
     B = "aaaab"    K = "abaab"    S = "baaab"
     C = "aaaba"    L = "ababa"    T = "baaba"
     D = "aaabb"    M = "ababb"    U = "baabb"
     E = "aabaa"    N = "abbaa"    W = "babaa"
     F = "aabab"    O = "abbab"    X = "babab"
     G = "aabba"    P = "abbba"    Y = "babba"
-    H = "aabbb"    Q = "abbbb"    Z = "babbb"
+    H = "aabbb"    Q = "abbbb"    Z = "babbb" =#
 
     # Take input
 
@@ -20,23 +20,34 @@ function baconian()
     secret = uppercase(secret)
     secret = replace(secret, "V" => "U")
     secret = replace(secret, "J" => "I")
+    secret = replace(secret, " " => "")
 
     println("Please input the normal text (5 times longer than the secret)")
     text = readline()
+    text = replace(text, " " => "")
     while length(text) != 5 * length(secret)
         println("Not 5 times the length of the secret. Try again")
         text = readline()
     end
     text = lowercase(text)
 
-    ciphertext = ""
+    cipherarray = []
 
     # Encrypt
     for i in 0:(length(secret)-1)
         letter = secret[i+1]
-
-
+        code = keys['Z' - letter + 1]
+        for j in 1:length(code)
+            if code[j] == 'b'
+                push!(cipherarray, uppercase(text[5i+j]))
+            else
+                push!(cipherarray, lowercase(text[5i+j]))
+            end
+        end
     end
+    # Covert array to String
+    ciphertext = join(cipherarray)
+    println(ciphertext)
 end
 
 baconian()
